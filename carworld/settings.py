b5fbe2441c5f7096c149dd8dae9dda459eb1db4c
Maneesh <manishreddy631@gmail.com>
2022-01-05 22:17:44 +0530

    completed module 6-cars-added

--- carworld/settings.py
+++ carworld/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     'cars.apps.CarsConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ckeditor',
+    'django.contrib.humanize',
 
 ]
 
