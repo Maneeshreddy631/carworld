dae90df558cf5f9c3364e45cf1273e5715a47b82
Maneesh <manishreddy631@gmail.com>
2022-01-27 16:32:14 +0530

    settigns change

--- carworld/settings.py
+++ carworld/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ckeditor',
+    'ckeditor_uploader',
     'django.contrib.humanize',
     'django.contrib.sites',
     'allauth',
